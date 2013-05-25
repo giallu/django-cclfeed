0d19e006ffff29609d1f2f29a0c9e431c163f8d8
Riccardo Vianello <riccardo.vianello@gmail.com>
2013-05-25 19:27:24 +0200

    improved management of charset and missing Date header.

--- cclfeed/management/commands/loadmessages.py
+++ cclfeed/management/commands/loadmessages.py
@@ -1,20 +1,21 @@
-from django.core.management.base import BaseCommand, CommandError
-from cclfeed.models import MessageData
 import ftplib
 import tempfile
 import mailbox
 import pytz
-import email
 import datetime
+import email
+
+from django.core.management.base import BaseCommand, CommandError
+
+from cclfeed.models import MessageData
+
 
 class Command(BaseCommand):
     help = 'Read messages from ftp.ccl.net and store them in the database'
 
     def handle(self, *args, **options):
         today = datetime.date.today()
-        days = [ today,
-                 today - datetime.timedelta(days=1)
-        ]
+        days = [today - datetime.timedelta(days=days) for days in range(5)]
 
         imported = 0
         messages = 0
@@ -26,28 +27,80 @@ class Command(BaseCommand):
                 self.stdout.write('reading file {}/{:02}'.format(directory, day.day))
                 f.cwd(directory)
                 f.retrbinary('RETR {:02}'.format(day.day), mbox.write)
+
+                parser = mailbox.mbox(mbox.name)
+                for i, msg in enumerate(parser):
+                    messages += 1
+                    datehdr = msg['Date']
+                    if datehdr is None:
+                        # without date it is not possible to build the
+                        # link to the original message on the CCL website
+                        continue
+                    timestamp = email.utils.mktime_tz(
+                        email.utils.parsedate_tz(get_header(datehdr)))
+                    date = datetime.datetime.fromtimestamp(timestamp, pytz.utc)
+                    body = get_body(msg) 
+                    _, created = MessageData.objects.get_or_create(
+                        msg_id=get_header(msg['Message-Id']),
+                        subject=get_header(msg['Subject']),
+                        sent_on=date,
+                        sender=get_header(msg['X-Original-From']),
+                        text=body.replace("\n","<br/>"),
+                        ordinal=i+1
+                        )
+                    if created:
+                        imported += 1
             except Exception as e:
                 self.stdout.write('Error: {}'.format(e))
 
-            parser = mailbox.mbox(mbox.name)
-            for i, msg in enumerate(parser):
-                messages = messages + 1
-                timestamp = email.utils.mktime_tz(email.utils.parsedate_tz(msg['Date']))
-                date = datetime.datetime.fromtimestamp( timestamp, pytz.utc )
-                message = ''
-                for part in msg.walk():
-                    if (part.get_content_maintype()=="text" and
-                        part.get_content_type()=="text/plain"):
-                        message = part.get_payload()
-                object, created = MessageData.objects.get_or_create(
-                    msg_id=msg['Message-Id'],
-                    subject=msg['Subject'],
-                    sent_on=date,
-                    sender=msg['X-Original-From'],
-                    text=message.replace("\n","<br/>"),
-                    ordinal=i+1
-                )
-                if created:
-                    imported = imported + 1
-
         self.stdout.write('%s messages read, % s new ' % (messages, imported))
+
+
+# some useful utility functions from 
+# http://ginstrom.com/scribbles/2007/11/19/parsing-multilingual-email-with-python/
+
+def get_header(header_text, default="ascii"):
+    """Decode the specified header"""
+
+    headers = email.header.decode_header(header_text)
+    header_sections = [
+        unicode(text, charset or default) for text, charset in headers
+        ]
+    return u"".join(header_sections)
+
+
+def get_charset(message, default="ascii"):
+    """Get the message charset"""
+
+    if message.get_content_charset():
+        return message.get_content_charset()
+    if message.get_charset():
+        return message.get_charset()
+    return default
+
+
+def get_body(message):
+    """Get the body of the email message"""
+
+    if message.is_multipart():
+        #get the plain text version only
+        text_parts = [
+            part for part in email.Iterators.typed_subpart_iterator(message,
+                                                                    'text',
+                                                                    'plain')
+            ]
+        body = []
+        for part in text_parts:
+            charset = get_charset(part, get_charset(message))
+            body.append(unicode(part.get_payload(decode=True), 
+                                charset,
+                                "replace"))
+
+        return u"\n".join(body).strip()
+
+    else: # if it is not multipart, the payload will be a string
+          # representing the message body
+        body = unicode(message.get_payload(decode=True),
+                       get_charset(message),
+                       "replace")
+        return body.strip()
